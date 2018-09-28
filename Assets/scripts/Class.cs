bf27e150f0ec5643f8dabb28cecb10acb398a4e3
Fran <teddies@ymail.com>
2018-09-28 12:37:05 -0400

    Revert "initial commit"
    This reverts commit 201652683b29cf011f64cd2743b4eb5dc316e0fa.
    
    Initial commit 2
    
    initial commit

---
+++ Assets/scripts/Class.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Class : MonoBehaviour {
+
+	//usage: put this on a capsule with a rigidbody, mouse look and WASD movement
+	public float moveSpeed;
+	//this variable will remember input and pass it to physics
+	Vector3 inputVector;
+		
+	void Update () {
+		//mouse input, mouse look !
+		//mouse DELTAS (different between frames
+		//be 0 when mouse isnt moving, this is not moues position
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+		
+		//rotate camera based on mouse input
+		transform.Rotate(0f, mouseX, 0f);
+		transform.Rotate(-mouseY, 0f, 0f);
+		
+		//WASD MOVEMENT 
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		//transform.position += transform.forward * vertical * moveSpeed;
+		//transform.position += transform.right * horizontal * moveSpeed; 
+
+		inputVector = transform.forward * vertical;
+		inputVector += transform.right * horizontal; 
+	}
+
+	//it runs every physics frame 
+	void FixedUpdate()
+	{
+		GetComponent<Rigidbody>().velocity = inputVector * moveSpeed + Physics.gravity * 0.69f;
+	}
+}
---
+++ Assets/scripts/rigidbodyVehicle.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidbodyVehicle : MonoBehaviour {
+
+//intent: this is a rigidbody based player controller for a boat or car
+//	usage: put this on a cube with a rigidbody
+
+	Rigidbody rbody;
+	private Vector2 inputVector;
+	public float paddleForce;
+	public float paddleRotation;
+
+	void Start()
+	{
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		//inputVector = new Vector2(horizontal, vertical); 
+		
+		if(Input.GetKeyDown(KeyCode.A)) {
+			rbody.AddForce(transform.forward * paddleForce * 1f, ForceMode.Impulse);
+			rbody.AddTorque(0f, paddleRotation * 10f, 0f);
+
+			}
+		if(Input.GetKeyDown(KeyCode.D)) {
+			rbody.AddForce(transform.forward * paddleForce * 1f, ForceMode.Impulse);
+			rbody.AddTorque(0f, -paddleRotation * 10f, 0f);
+
+		}
+
+	
+	}
+
+	//fixedupdate runs every physics frame
+	//to change physics framerate, go to Edit > Project settings T	
+	void FixedUpdate()
+	{
+		
+		//forward and backward thrust
+		//rbody.AddForce(transform.forward * inputVector.y * 10f); 
+		
+		//left/right 
+	//	rbody.AddTorque(0f, inputVector.x * 5f, 0f);
+
+	}
+}
