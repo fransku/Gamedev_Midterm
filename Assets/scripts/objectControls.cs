5b86532382e0ba4fd9128ed8fe5ddabac454a2ba
Fran <teddies@ymail.com>
2018-10-01 11:16:56 -0400

    thorwing object works

--- Assets/scripts/objectControls.cs
+++ Assets/scripts/objectControls.cs
@@ -8,38 +8,43 @@ public class objectControls : MonoBehaviour
 	public Transform player;
 	public Transform playerCam;
 	public float throwForce = 1;
-	 bool hasPlayer = false; 
+	bool hasPlayer = false;
 	bool isCarried = false;
 	private bool touched = false;
-	
-	
-	void Start () {
-		
+
+
+	void Start()
+	{
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		//calculate distance between player and gameobject
-		float dist = Vector3.Distance(gameObject.transform.position, player.position)
+		float dist = Vector3.Distance(gameObject.transform.position, player.position);
 		//if distance is less than 3.5f, we can pick up object
-		if (dist <= 3.5f)
+		if (dist <= 5.5f)
 		{
+			Debug.Log("player in distance");
 			hasPlayer = true;
 		}
 		else
 		{
-			hasPlayer = false; 
+			hasPlayer = false;
 		}
 
-		if (hasPlayer && Input.GetKeyDown("Space"))
+		if (hasPlayer && Input.GetKeyDown(KeyCode.Space))
 		{
 			GetComponent<Rigidbody>().isKinematic = true;
-			transform.parent = null;
-			isCarried = true; 
-			
+			transform.parent = playerCam;
+			isCarried = true;
+
 		}
+
 		if (isCarried)
 		{
+			//if touching other object or wall, it just falls
 			if (touched)
 			{
 				GetComponent<Rigidbody>().isKinematic = false;
@@ -47,14 +52,25 @@ public class objectControls : MonoBehaviour
 				isCarried = false;
 				touched = false;
 			}
-			if (Input.GetMouseButtonDown(0))
+
+			if (Input.GetMouseButtonDown(0)) //primary button to throw things away
 			{
 				GetComponent<Rigidbody>().isKinematic = false;
 				transform.parent = null;
 				isCarried = false;
 				GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+			}
+			else if (Input.GetMouseButtonDown(1)) //secondary button drops object, no force
+			{
+			//	GetComponent<RigidBody>().isKinematic = false;
+				transform.parent = null;
+				isCarried = false; 
+			}
+		}
 	}
 
+
+	//two objects dont touch at once
 	void OnTriggerEnter()
 	{
 		if (isCarried)
@@ -62,5 +78,9 @@ public class objectControls : MonoBehaviour
 			touched = true;
 		}
 	}
-
 }
+
+	
+	
+
+
